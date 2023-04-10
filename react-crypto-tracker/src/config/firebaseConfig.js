bfb192e5c60e51c887e866bd6dfd517c28984a81
Ravi <ravindrak1474@gmail.com>
2023-04-10 14:01:11 +0530

    first  commit

---
+++ react-crypto-tracker/src/config/firebaseConfig.js
@@ -0,0 +1,15 @@
+
+const firebaseConfig = {
+       apiKey: "AIzaSyA6IhFk4g47OeR8LGCoB_Ytgb9D-Rci62k",
+      authDomain: "crypto-current-tracker.firebaseapp.com",
+      projectId: "crypto-current-tracker",
+     storageBucket: "crypto-current-tracker.appspot.com",
+     messagingSenderId: "377611133240",
+     appId: "1:377611133240:web:72460cbc11bc604f1fe1cd"
+    };
+  
+     export default firebaseConfig;
+
+
+
+
